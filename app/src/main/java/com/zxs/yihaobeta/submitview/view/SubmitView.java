5d5f961352356ce400de1f953fb812b070cc6532
yihaobeta <yihaobeta@163.com>
2017-10-26 19:00:00 +0800

    1.增加自定义属性支持 2.添加相关注释

--- app/src/main/java/com/zxs/yihaobeta/submitview/view/SubmitView.java
+++ app/src/main/java/com/zxs/yihaobeta/submitview/view/SubmitView.java
@@ -4,18 +4,22 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.ValueAnimator;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.RectF;
 import android.support.annotation.Dimension;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.AccelerateInterpolator;
+import com.zxs.yihaobeta.submitview.R;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Created by yihaobeta on 2017/10/25.
@@ -23,40 +27,60 @@ import android.view.animation.AccelerateInterpolator;
 
 public class SubmitView extends View {
 
-  private Context context;
+  private Context mContext;
 
-  //默认高度
-  private float mViewHeight = 150f;
-  //默认宽度
-  private float mViewWidth = mViewHeight * 5;
+  //实际测量高度
+  private float mViewHeight;
+  //实际测量宽度
+  private float mViewWidth;
 
-  //整个View的外接矩形
+  //整个绘制View的外接矩形
   private RectF mViewRect = new RectF();
   //动画进行过程中的外切矩形
   private RectF mAnimRect = new RectF();
 
   //背景画笔
   private Paint mBgPaint;
+  private int mBgColor;
   //文本画笔
   private Paint mTextPaint;
-  private float mTextSize = 50;
+  //文本颜色
+  private int mTextColor;
+  //字体大小
+  private float mTextSize;
   //对勾画笔
   private Paint mTickPaint;
+  private int mTickColor;
+  //收缩动画
   private ValueAnimator mShrinkAnim;
-  private long mShrinkDuration = 500;
+  private long mShrinkAnimDuration = 500;
+  //当前的状态
   private Status mStatus = Status.NORMAL;
-  private float mCurrLength = 0;
+  //收缩动画过程中的view长度值
+  private float mCurViewLengthInAnim = 0;
+  //提交过程动画
   private ValueAnimator mPrepareRotateAnim;
+  //提交成功动画
   private ValueAnimator mCompletedAnim;
   private int mCompletedAnimDuration = 1000;
   private long mPrepareRotateAnimDuration = 2000;
-  private float mPrepareRotateAngle;
-  private float mPrepareRotateAnimSpeed = 10;
+  //画布旋转角度
+  private float mRotateAngle;
+  //画布旋转步进值
+  private float mRotateAnimSpeed = 10;
+  //提交成功动画的圆半径值
   private float mCompleteAnimCircleRadio;
 
   //对勾的路径
   private Path mTickPath = new Path();
   private String mText = "Submit";
+
+  //常量定义
+  //view的默认长宽比
+  private static final int ASPECT_RATIO = 5;
+  private static final float VIEW_HEIGHT = 150f;
+  private static final float VIEW_WIDTH = VIEW_HEIGHT * ASPECT_RATIO;
+
 
   public SubmitView(Context context) {
     this(context, null);
@@ -73,41 +97,62 @@ public class SubmitView extends View {
     initAnim();
   }
 
+  /**
+   * 初始化画笔
+   */
   private void initPaint() {
     mBgPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    mBgPaint.setColor(Color.BLUE);
+    mBgPaint.setColor(mBgColor);
     mBgPaint.setStyle(Paint.Style.FILL);
 
     mTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    mTextPaint.setColor(Color.WHITE);
+    mTextPaint.setColor(mTextColor);
     mTextPaint.setStyle(Paint.Style.FILL_AND_STROKE);
     mTextPaint.setStrokeWidth(2);
     mTextPaint.setTextSize(mTextSize);
     mTextPaint.setTextAlign(Paint.Align.CENTER);
 
     mTickPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    mTickPaint.setColor(Color.WHITE);
+    mTickPaint.setColor(mTickColor);
     mTickPaint.setStyle(Paint.Style.STROKE);
     mTickPaint.setStrokeWidth(8);
   }
 
+  /**
+   * 获取自定义属性
+   * @param context
+   * @param attrs
+   */
   private void initAttrs(Context context, AttributeSet attrs) {
-    this.context = context;
+    this.mContext = context;
+    TypedArray ta = getContext().obtainStyledAttributes(attrs, R.styleable.SubmitView);
+    mTextSize = ta.getInteger(R.styleable.SubmitView_textSize,50);
+    mBgColor = ta.getColor(R.styleable.SubmitView_backgroundColor,Color.BLUE);
+    mText = ta.getString(R.styleable.SubmitView_text);
+    if(mText == null || mText.length()<=0){
+      mText = "Submit";
+    }
+
+    mTextColor = ta.getColor(R.styleable.SubmitView_textColor,Color.WHITE);
+    mTickColor = ta.getColor(R.styleable.SubmitView_tickColor, Color.WHITE);
+    ta.recycle();
   }
 
   @Override
   protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
     super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-    float width = getRealSize(mViewWidth, widthMeasureSpec, true);
-    float height = getRealSize(mViewHeight, heightMeasureSpec, false);
+    float width = getRealSize(VIEW_WIDTH, widthMeasureSpec, true);
+    float height = getRealSize(VIEW_HEIGHT, heightMeasureSpec, false);
 
     if (width <= height) {
-      height = width/5;
+      height = width/ASPECT_RATIO;
     }
     setMeasuredDimension((int) width, (int) height);
     mViewRect.set(getPaddingLeft(), getPaddingTop(), getWidth() - getPaddingLeft(),
         getHeight() - getPaddingTop());
-    mCurrLength = mViewRect.width();
+    mCurViewLengthInAnim = mViewRect.width();
+    mViewHeight = getMeasuredHeight();
+    mViewWidth = getMeasuredWidth();
   }
 
   private float getRealSize(float defaultSize, int measureSpec, boolean isWidth) {
@@ -135,22 +180,29 @@ public class SubmitView extends View {
   @Override
   protected void onDraw(Canvas canvas) {
     super.onDraw(canvas);
+    //绘制正常状态
     if (mStatus == Status.NORMAL) {
+      //画初始的圆角矩形
       canvas.drawRoundRect(mViewRect, mViewRect.height() / 2, mViewRect.height() / 2, mBgPaint);
       Paint.FontMetrics fm = mTextPaint.getFontMetrics();
-      float y = getHeight() / 2 + (fm.descent - fm.ascent) / 2 - fm.descent;
-      canvas.drawText(mText, getWidth() / 2, y, mTextPaint);
-    } else if (mStatus == Status.START) {
+      float y = mViewHeight / 2 + (fm.descent - fm.ascent) / 2 - fm.descent;
+      canvas.drawText(mText, mViewWidth / 2, y, mTextPaint);
+    }
 
-      float mLeft = mViewRect.left + ((mViewRect.width() - mCurrLength) / 2);
-      float mRight = mLeft + mCurrLength;
-      mAnimRect.set(mLeft, mViewRect.top, mRight, mViewRect.bottom);
-      //RectF temp = new RectF();
+    //绘制开始submit作业时的收缩动画
+    else if (mStatus == Status.START) {
+
+      float left = mViewRect.left + ((mViewRect.width() - mCurViewLengthInAnim) / 2);
+      float right = left + mCurViewLengthInAnim;
+      mAnimRect.set(left, mViewRect.top, right, mViewRect.bottom);
       canvas.drawRoundRect(mAnimRect, mViewRect.height() / 2, mViewRect.height() / 2, mBgPaint);
-    } else if (mStatus == Status.SUBMITTING) {
+    }
+
+    //绘制正在submit时的加载动画
+    else if (mStatus == Status.SUBMITTING) {
       canvas.drawCircle(mAnimRect.centerX(), mAnimRect.centerY(), mAnimRect.height() / 2, mBgPaint);
       canvas.save();
-      canvas.rotate(mPrepareRotateAngle, mAnimRect.centerX(), mAnimRect.centerY());
+      canvas.rotate(mRotateAngle, mAnimRect.centerX(), mAnimRect.centerY());
       float miniCircleX = mAnimRect.centerX();
       float minCircleR = mAnimRect.width() / 15;
       float minCircleY = mAnimRect.top + mAnimRect.height() / 5;
@@ -160,23 +212,33 @@ public class SubmitView extends View {
       canvas.rotate(120, mAnimRect.centerX(), mAnimRect.centerY());
       canvas.drawCircle(miniCircleX, minCircleY, minCircleR, mTextPaint);
       canvas.restore();
-    } else if (mStatus == Status.COMPLETE) {
+    }
+
+    //绘制submit完成时的圆缩放动画
+    else if (mStatus == Status.COMPLETE) {
       canvas.drawCircle(mAnimRect.centerX(), mAnimRect.centerY(), mCompleteAnimCircleRadio,
           mBgPaint);
-    } else if (mStatus == Status.END) {
+    }
+
+    //绘制最后的对勾
+    else if (mStatus == Status.END) {
       canvas.drawCircle(mAnimRect.centerX(), mAnimRect.centerY(), mAnimRect.height() / 2, mBgPaint);
       canvas.drawPath(mTickPath, mTickPaint);
     }
   }
 
+  /**
+   * 初始化动画
+   */
   private void initAnim() {
     mShrinkAnim = ValueAnimator.ofFloat(0, 1);
-    mShrinkAnim.setDuration(mShrinkDuration);
+    mShrinkAnim.setDuration(mShrinkAnimDuration);
     mShrinkAnim.setInterpolator(new AccelerateInterpolator());
     mShrinkAnim.addListener(new AnimatorListenerAdapter() {
       @Override
       public void onAnimationStart(Animator animation) {
-        mCurrLength = mViewRect.width();
+        //动画开始前的初始view宽度为整个view的宽度
+        mCurViewLengthInAnim = mViewRect.width();
       }
 
       @Override
@@ -189,9 +251,9 @@ public class SubmitView extends View {
     mShrinkAnim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
       @Override
       public void onAnimationUpdate(ValueAnimator animation) {
-        mCurrLength = mViewRect.width() * (1 - animation.getAnimatedFraction());
-        if (mCurrLength <= mViewRect.height()) {
-          mCurrLength = mViewRect.height();
+        mCurViewLengthInAnim = mViewRect.width() * (1 - animation.getAnimatedFraction());
+        if (mCurViewLengthInAnim <= mViewRect.height()) {
+          mCurViewLengthInAnim = mViewRect.height();
           mShrinkAnim.cancel();
         }
         invalidate();
@@ -211,11 +273,13 @@ public class SubmitView extends View {
     mPrepareRotateAnim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
       @Override
       public void onAnimationUpdate(ValueAnimator animation) {
-        mPrepareRotateAngle += mPrepareRotateAnimSpeed;
+        //旋转角度递进
+        mRotateAngle += mRotateAnimSpeed;
         invalidate();
       }
     });
 
+    //submit完成时的动画
     mCompletedAnim = ValueAnimator.ofFloat(1, 0, 1);
     mCompletedAnim.setDuration(mCompletedAnimDuration);
     mCompletedAnim.setInterpolator(new AccelerateDecelerateInterpolator());
@@ -224,6 +288,7 @@ public class SubmitView extends View {
       public void onAnimationEnd(Animator animation) {
         super.onAnimationEnd(animation);
 
+        //确定对勾在圆内的关键点坐标
         int centerX = (int) mAnimRect.centerX();
         int centerY = (int) mAnimRect.centerY();
         int radio = (int) (mAnimRect.height() / 2);
@@ -239,11 +304,13 @@ public class SubmitView extends View {
       @Override
       public void onAnimationUpdate(ValueAnimator animation) {
         mCompleteAnimCircleRadio = mAnimRect.height() / 2 * (float) animation.getAnimatedValue();
-        System.out.println("mCompleteAnimCircleRadio = " + animation.getAnimatedValue());
       }
     });
   }
 
+  /**
+   * 开始submit操作时调用
+   */
   public void submit() {
     cancelAllAnim();
     mStatus = Status.START;
@@ -251,12 +318,18 @@ public class SubmitView extends View {
     this.setClickable(false);
   }
 
+  /**
+   * submit完成时调用
+   */
   public void submitCompleted() {
     mStatus = Status.COMPLETE;
     mCompletedAnim.start();
     this.setClickable(true);
   }
 
+  /**
+   * 恢复初始状态
+   */
   public void cancel() {
     mStatus = Status.NORMAL;
     this.setClickable(true);
@@ -274,16 +347,39 @@ public class SubmitView extends View {
     mPrepareRotateAnim.cancel();
   }
 
-  public SubmitView setText(String text) {
+  public SubmitView setText(@NonNull String text) {
     this.mText = text;
+    invalidate();
     return this;
   }
 
-  public SubmitView setTextSize(float mTextSize) {
+  public SubmitView setTextSize(@NonNull float textSize) {
 
-    this.mTextSize = TypedValue.applyDimension(Dimension.SP, mTextSize,
-        context.getResources().getDisplayMetrics());
+    this.mTextSize = TypedValue.applyDimension(Dimension.SP, textSize,
+        mContext.getResources().getDisplayMetrics());
     mTextPaint.setTextSize(this.mTextSize);
+    invalidate();
+    return this;
+  }
+
+  public SubmitView setTextColor(@NotNull int textColor){
+    this.mTextColor = textColor;
+    mTextPaint.setColor(this.mTextColor);
+    invalidate();
+    return this;
+  }
+
+  public SubmitView setBgColor(@NotNull int bgColor){
+    this.mBgColor = bgColor;
+    this.mBgPaint.setColor(this.mBgColor);
+    invalidate();
+    return this;
+  }
+
+  public SubmitView setTickColor(@NotNull int tickColor){
+    this.mTickColor = tickColor;
+    this.mTickPaint.setColor(this.mTickColor);
+    invalidate();
     return this;
   }
 
